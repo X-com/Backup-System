573782859f688625f22a44c89ab347409d05d5cf
unknown <sahandhesar@gmail.com>
2020-06-13 19:24:58 +0200

    Updates

---
+++ precache-manifest.2bfff8a079a6ade801805ceaea17f6c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "293e1a710b8789d3c37cdb6bc9e338f6",
+    "url": "/Backup-System/index.html"
+  },
+  {
+    "revision": "d19e145a0f2c72415a49",
+    "url": "/Backup-System/static/css/main.eeb8a0fa.chunk.css"
+  },
+  {
+    "revision": "e7a68fb1e4f494d7a156",
+    "url": "/Backup-System/static/js/2.df32ba0b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Backup-System/static/js/2.df32ba0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d19e145a0f2c72415a49",
+    "url": "/Backup-System/static/js/main.670a887f.chunk.js"
+  },
+  {
+    "revision": "b8a286487db90c92c9ce",
+    "url": "/Backup-System/static/js/runtime-main.fbe29bd7.js"
+  }
+]);
